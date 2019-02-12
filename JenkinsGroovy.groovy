f500014f1f6425ad7575609cd4ec89455c325db4
cnehru <47534342+cnehru@users.noreply.github.com>
2019-02-12 23:27:38 +0530

    Update JenkinsGroovy.groovy

--- JenkinsGroovy.groovy
+++ JenkinsGroovy.groovy
@@ -1,59 +1,43 @@
+import java.util.Timer
+import java.util.TimerTask
 
+import groovy.lang.Closure
 
-def mail() {
-
-	// Recipient's email ID needs to be mentioned.
-	def receivers = "cnehru22@gmail.com"
-
-	// Sender's email ID needs to be mentioned
-	def sender = "web@gmail.com"
-
-	// Assuming you are sending email from localhost
-	def host = "smtp.gmail.com"
-
-	def subject = "Test Email Subject"
-
-	def text = "Test Email"
-
-	Properties props = new Properties();
-	props.put("mail.smtp.host", host)
-	props.put("mail.transport.protocol", "smtp");
-	props.put("mail.host", "smtp.gmail.com");
-	props.put("mail.smtp.auth", "true");
-	props.put("mail.smtp.port", "465");
-	//props.put("mail.debug", "true");
-	props.put("mail.smtp.socketFactory.port", "465");
-	props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
-	props.put("mail.smtp.socketFactory.fallback", "false");
-
-	try{
-		Session session = Session.getDefaultInstance(props,
-				new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication("cnehru22@gmail.com","lybuvqbldeeaswnf");
-					}
-				});
-
-		//session.setDebug(true);
-		Transport transport = session.getTransport();
-		InternetAddress addressFrom = new InternetAddress(sender);
-
-		MimeMessage message = new MimeMessage(session);
-		message.setSender(addressFrom);
-		message.setSubject(subject);
-		message.setContent(text, "text/plain");
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(receivers));
-
-		transport.connect();
-		Transport.send(message);
-		transport.close();
-	}catch(Exception e){
-		e.printStackTrace()
+class GroovyTimerTask extends TimerTask {
+	Closure closure
+	void run() {
+		closure()
 	}
+}
 
+class TimerMethods {
+	static TimerTask runEvery(Timer timer, long delay, long period, Closure codeToRun) {
+		TimerTask task = new GroovyTimerTask(closure: codeToRun)
+		//timer.schedule task, delay, period
+		//task
+
+		//Get the Date corresponding to 11:01:00 pm today.
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, 15);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		Date time = calendar.getTime();
+
+		timer = new Timer();
+		timer.schedule(task, time);
+
+
+	}
+}
+
+use (TimerMethods) {
+	def timer = new Timer()
+	def task = timer.runEvery(1000, (60000 * 60 * 24)) {//1 DAY
+		println "Task executed at ${new Date()}." }
+	println "Current date is ${new Date()}."
 }
 
 return [
-	mail: this.mail()
+	mail: this.use()
 ]
 
