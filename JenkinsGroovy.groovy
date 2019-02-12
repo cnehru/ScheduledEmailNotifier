9e426453edeb7d6a14f2159b7b4d23dbf333a8bf
cnehru <47534342+cnehru@users.noreply.github.com>
2019-02-13 00:00:44 +0530

    Update JenkinsGroovy.groovy

--- JenkinsGroovy.groovy
+++ JenkinsGroovy.groovy
@@ -24,7 +24,7 @@ def mail() {
 	props.put("mail.host", "smtp.gmail.com");
 	props.put("mail.smtp.auth", "true");
 	props.put("mail.smtp.port", "465");
-	//props.put("mail.debug", "true");
+	props.put("mail.debug", "true");
 	props.put("mail.smtp.socketFactory.port", "465");
 	props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
 	props.put("mail.smtp.socketFactory.fallback", "false");
